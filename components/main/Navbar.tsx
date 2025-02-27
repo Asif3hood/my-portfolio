d7c0b4b2999106469a8d9a277a9fd7876ddc14ed
Asif3hood <110968875+Asif3hood@users.noreply.github.com>
2025-02-27 17:42:57 +0530

    Update Navbar.tsx

--- components/main/Navbar.tsx
+++ components/main/Navbar.tsx
@@ -14,8 +14,9 @@ const Navbar = () => {
         autoClose={3000}
       />
 
-      <div className="w-full h-[65px] fixed top-0 shadow shadow-[#2A0E61]/50 bg-[#03001417] backdrop-blur-md z-[9999] px-20">
+      <div className="w-full h-[65px] fixed top-0 shadow shadow-[#2A0E61]/50 bg-[#03001417] backdrop-blur-md z-[9999] px-5 md:px-20">
         <div className="w-full h-full flex flex-row items-center justify-between m-auto px-[10px]">
+          {/* Logo */}
           <a href="#about-me" className="h-auto w-auto flex flex-row items-center">
             <Image
               src="/NavLogo.png"
@@ -30,8 +31,9 @@ const Navbar = () => {
             </span>
           </a>
 
-          <div className="w-[500px] h-full flex flex-row items-center justify-between md:mr-20">
-            <div className="flex items-center justify-between w-full h-auto border border-[#7042f861] bg-[#0300145e] mr-[15px] px-[20px] py-[10px] rounded-full text-gray-200">
+          {/* Navigation Links */}
+          <div className="max-w-[500px] w-full h-full flex flex-row items-center justify-between md:mr-20">
+            <div className="flex flex-wrap items-center justify-center w-full h-auto border border-[#7042f861] bg-[#0300145e] mr-[15px] px-[20px] py-[10px] rounded-full text-gray-200 gap-x-6">
               <a href="#about-me" className="cursor-pointer">
                 About
               </a>
@@ -41,9 +43,13 @@ const Navbar = () => {
               <a href="#projects" className="cursor-pointer">
                 Projects
               </a>
+              <a href="#contact" className="cursor-pointer">
+                Contact
+              </a>
             </div>
           </div>
 
+          {/* Social Icons */}
           <div className="flex flex-row gap-5">
             {Socials.map((social) => (
               <Image
@@ -60,5 +66,7 @@ const Navbar = () => {
     </>
   );
 };
+
+export default Navbar;
 
 export default Navbar;
