8383ed710b3dea5c512086294b8c574afbc59f1b
Asif3hood <110968875+Asif3hood@users.noreply.github.com>
2025-02-27 17:31:08 +0530

    Update Navbar.tsx

--- components/main/Navbar.tsx
+++ components/main/Navbar.tsx
@@ -14,14 +14,14 @@ const Navbar = () => {
         autoClose={3000}
       />
 
-      <div className="w-full h-[65px] fixed top-0 shadow-lg shadow-[#2A0E61]/50 bg-[#03001417] backdrop-blur-md z-[9999] px-10">
+      <div className="w-full h-[65px] fixed top-0 shadow shadow-[#2A0E61]/50 bg-[#03001417] backdrop-blur-md z-[9999] px-10">
         <div className="w-full h-full flex flex-row items-center justify-between m-auto px-[10px]">
           <a href="#about-me" className="h-auto w-auto flex flex-row items-center">
             <Image
               src="/NavLogo.png"
               alt="logo"
-              width={70}
-              height={70}
+              width={90}
+              height={90}
               className="cursor-pointer hover:animate-slowspin"
             />
 
